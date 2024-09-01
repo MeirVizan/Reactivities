872575d78ff210475e9af0a54e9a2193fc89ea5a
MeirVizan <meir15102@gmail.com>
2024-09-01 21:02:10 +0300

    End of section 10

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 using Application.Activities;
 using Application.Core;
 using MediatR;
@@ -15,6 +16,9 @@ builder.Services.AddApplicationServices(builder.Configuration);
 
 
 var app = builder.Build();
+
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
